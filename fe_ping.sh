b359dbfc6fed363471a384a9e94243d95b9e0ef1
Andre Catarino <ac639@njit.edu>
2019-05-12 13:25:17 -0400

    front end ping

---
+++ fe_ping.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+#Host is master database
+HOST=192.168.1.11
+ping -c1 $HOST 1>/dev/null 2>/dev/null
+SUCCESS=$?
+
+if [ $SUCCESS -eq 0 ]
+then
+        echo "$HOST has replied"
+else
+        echo "$HOST didn't reply"
+
+        ifconfig enp0s9 192.168.1.12 netmask 255.255.255.0 up
+        #systemctl restart 490sysd
+        systemctl restart apache2
+fi
+#EOF
+
